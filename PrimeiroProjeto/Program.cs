c2dcbba4ba6612278fe3ae61418040aa717c8d71
Da Silva <111332935+edsuuu@users.noreply.github.com>
2023-12-28 14:09:43 -0300

    att

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Toda sintaxe de definição de variavel funciona com 
+            //<tipo da variavel> <nome da vriavel> = <valor da variavel>
+
+            //Para valores inteiros usar int
+            int idade = 25;
+
+            //Para valores quebrados usamos double
+            double altura = 1.75;
+            
+            //Para valores de 1 caracter usamos char
+            char sexo = 'M';
+
+
+            Console.WriteLine($"A idade dessa pessoa é {idade} e tem altura de {altura} é do sexo {sexo}");
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
